9af6c81eb5ecbed118910cc1961a799667d7434c
Олега <degoleg@yandex.ru>
2023-01-05 22:11:07 +0400

    primer 2

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/filter')
+def filter_page():
+  from_value = request.args['from']
+  to_value = request.args['to']
+  return f'Ищем в диапазоне от {from_value} до {to_value}'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
