d8483ef6189232c0166d4de384329c37f6bf65f7
Олега <degoleg@yandex.ru>
2023-01-06 22:19:09 +0400

    Handling data from the request body

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/filter')
-def filter_page():
-  from_value = request.args['from']
-  to_value = request.args['to']
-  return f'Ищем в диапазоне от {from_value} до {to_value}'
+@app.route('/')
+def form_page():
+    return render_template('search.html')
 
+@app.route('/search')
+def search_page():
+    s = request.args['s']
+    return f'Вы ввели слово {s}'
 
 
 if __name__ == '__main__':
